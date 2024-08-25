fb7b5d544b767e3ca246f31926840b577f3babb2
ziomCodes <ziomcode@gmail.com>
2024-08-25 14:18:39 -0700

    js for index page

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.waitlist-btn').addEventListener('click', () => {
+    alert('You have joined the waitlist!');
+});
+document.querySelector('.waitlist-btn1').addEventListener('click', () => {
+    alert('You have joined the waitlist!');
+});
+
